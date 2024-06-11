d56464dcea73a513d36252d9c3c870208959b363
Vedant Kulkarni <132735080+Vedant-K16@users.noreply.github.com>
2024-06-12 00:28:03 +0530

    Update profit_aggregations.py

--- aggregation/profit_aggregations.py
+++ aggregation/profit_aggregations.py
@@ -12,23 +12,10 @@ orders_clean = spark.read.table("orders_clean")
 customers_clean = spark.read.table("customer_clean")
 products_clean = spark.read.table("products_clean")
 
-# profit enriched table
-profit = (orders_clean.alias("order")
-                   .join(customers_clean.alias("customer"),["Customer_id"])
-                   .join(products_clean.alias("product"),["Product_ID"])
-                   .groupBy(
-                               "customer.customer_name",
-                               "product.category",
-                               "product.`Sub-Category`",
-                               year(col("order.order_date")).alias("order_year")
-                               )
-                   .agg(
-                       round(sum("order.profit"),2).alias("profit")
-                   )
-                   )
+profit_df = profit_aggregation(orders_clean,customers_clean,products_clean)
 
 create_table(
-    df=profit,
+    df=profit_df,
     table_format="delta",
     table_name="profit",
     mode="overwrite"
